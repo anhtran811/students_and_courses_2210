3446f327b45e0fbd3e3572ba0a48b1a2b849355e
Anhnhi Tran <anhtran811@gmail.com>
2022-10-27 10:01:52 -0600

    Created gradebook file and initialized

---
+++ lib/gradebook.rb
@@ -0,0 +1,17 @@
+class Gradebook
+  def initialize(instructor)
+  end
+end
+
+
+
+
+
+# You have been contracted by the University of Denver to write a program 
+# that models gradebooks for its courses. Specifically, 
+# it would like you to implement the following features:
+
+# Each gradebook has courses and can list those courses
+# A gradebook can list all students in its courses
+# A gradebook can list all students with a grade below a given threshold
+# Build upon your code from the first two iterations to complete this task.
