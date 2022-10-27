da9ec77ecb2eb61e670726f90fdcfa92c63f4a5b
Anhnhi Tran <anhtran811@gmail.com>
2022-10-27 10:52:08 -0600

    Iteration 3 with erros for listing students in course)

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -9,16 +9,11 @@ attr_reader :instructor, :courses
   def add_course(course)
     @courses << course
   end
+
+  def list_students(student)
+  end
 end
 
 
-
-
-
-# You have been contracted by the University of Denver to write a program 
-# that models gradebooks for its courses. Specifically, 
-# it would like you to implement the following features:
-
 # A gradebook can list all students in its courses
 # A gradebook can list all students with a grade below a given threshold
-# Build upon your code from the first two iterations to complete this task.
--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -21,7 +21,7 @@ RSpec.describe Gradebook do
     expect(gradebook.courses).to eq([])
   end
 
-  it 'can have courses' do
+  it 'can add courses' do
     gradebook = Gradebook.new("Corey")
     calculus = Course.new("Calculus", 2)
     english = Course.new("English", 3)
@@ -30,5 +30,22 @@ RSpec.describe Gradebook do
     gradebook.add_course(english)
 
     expect(gradebook.courses).to eq([calculus, english])
+  end
+
+  it 'can list students in the courses' do
+    gradebook = Gradebook.new("Corey")
+    calculus = Course.new("Calculus", 2)
+    english = Course.new("English", 1)
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+    student3 = Student.new({name: "Amy", age: 22})
+
+    calculus.enroll(student1)
+    calculus.enroll(student2)
+    english.enroll(student3)
+
+    expect(gradebook.add_course(calculus)).to eq([student1, student2])
+    expect(gradebook.add_course(english)).to eq([student3])
+  end
   end
 end
