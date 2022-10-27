b9c30e7006ed8aa3f6c359951d3c05f1addfc1fa
Anhnhi Tran <anhtran811@gmail.com>
2022-10-27 09:56:09 -0600

    Iteration 2 with error on become full

--- lib/course.rb
+++ lib/course.rb
@@ -1,9 +1,26 @@
 class Course
-attr_reader :name, :capacity, :students
+attr_reader :name, :capacity, :students, :full
 
   def initialize(name, capacity)
     @name = name
     @capacity = capacity
     @students = []
+    @full = false
+  end
+
+  def full?
+    @full
+  end
+
+  def enroll(student)
+    @students << student
+  end
+
+  def full
+    if @capacity = @students.length
+      return true
+    else
+      @full
+    end
   end
 end
