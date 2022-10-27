9bb140cb0f1a1ce1c05134ba638f8f3c84f9b39c
Anhnhi Tran <anhtran811@gmail.com>
2022-10-27 09:56:33 -0600

    Iteartion 2 with test become full written but not passing

--- spec/course_spec.rb
+++ spec/course_spec.rb
@@ -25,27 +25,33 @@ RSpec.describe Course do
 
     expect(course.students).to eq([])
   end
+
+  it 'is not full to start' do
+    course = Course.new("Calculus", 2) 
+
+    expect(course.full?).to eq(false)
+  end
+
+  it 'can enroll students' do
+    course = Course.new("Calculus", 2) 
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+
+    course.enroll(student1)
+    course.enroll(student2)
+
+    expect(course.students).to eq([student1, student2])
+  end
+
+  it 'can become full' do
+    course = Course.new("Calculus", 2) 
+    student1 = Student.new({name: "Morgan", age: 21})
+    student2 = Student.new({name: "Jordan", age: 29})
+
+    course.enroll(student1)
+    course.enroll(student2)
+
+    expect(course.capacity).to eq(2)
+    expect(course.full?).to eq(true)
+  end
 end
-
-
-# course.students
-# # => []
-
-# course.full?
-# # => false
-
-# student1 = Student.new({name: "Morgan", age: 21})
-# # => #<Student:0x00007fa0a80ae588...>
-
-# student2 = Student.new({name: "Jordan", age: 29})    
-# # => #<Student:0x00007fa0a814f4d8...>
-
-# course.enroll(student1)    
-
-# course.enroll(student2)    
-
-# course.students
-# # => [#<Student:0x00007fa0a80ae588...>, #<Student:0x00007fa0a814f4d8...>]
-
-# course.full?
-# # => true
