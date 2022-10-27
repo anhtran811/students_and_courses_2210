3f1ab45deb95de7f63a481074a8e214c145a382f
Anhnhi Tran <anhtran811@gmail.com>
2022-10-27 10:11:25 -0600

    Wrote tests for adding courses with passing tests

--- spec/gradebook_spec.rb
+++ spec/gradebook_spec.rb
@@ -8,4 +8,27 @@ RSpec.describe Gradebook do
 
     expect(gradebook).to be_a(Gradebook)
   end
+
+  it 'has an instructor' do
+    gradebook = Gradebook.new("Corey")
+
+    expect(gradebook.instructor).to eq("Corey")
+  end
+
+  it 'starts without courses' do
+    gradebook = Gradebook.new("Corey")
+
+    expect(gradebook.courses).to eq([])
+  end
+
+  it 'can have courses' do
+    gradebook = Gradebook.new("Corey")
+    calculus = Course.new("Calculus", 2)
+    english = Course.new("English", 3)
+
+    gradebook.add_course(calculus)
+    gradebook.add_course(english)
+
+    expect(gradebook.courses).to eq([calculus, english])
+  end
 end
