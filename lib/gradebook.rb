320fea2fc5ab8ed864ac3f9c90ac40800eefa805
Anhnhi Tran <anhtran811@gmail.com>
2022-10-27 10:10:18 -0600

    Add course method passed

--- lib/gradebook.rb
+++ lib/gradebook.rb
@@ -1,5 +1,13 @@
 class Gradebook
+attr_reader :instructor, :courses
+
   def initialize(instructor)
+    @instructor = instructor
+    @courses = []
+  end
+
+  def add_course(course)
+    @courses << course
   end
 end
 
@@ -11,7 +19,6 @@ end
 # that models gradebooks for its courses. Specifically, 
 # it would like you to implement the following features:
 
-# Each gradebook has courses and can list those courses
 # A gradebook can list all students in its courses
 # A gradebook can list all students with a grade below a given threshold
 # Build upon your code from the first two iterations to complete this task.
